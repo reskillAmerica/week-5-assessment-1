547791f400d340491fa6a2abbd21977f00f07218
henok Gebremedhn <henoklwi@gmail.com>
2021-05-06 21:19:03 -0700

    commite complited

---
+++ main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// const myDisplay = (text) => {
+//   document.getElementById('demo').innerHTML = text;
+// };
+
+// const myPromise = new Promise((resolve, reject) => {
+//   let x = 210;
+//   if (x === 10) {
+//     resolve('OK');
+//   } else {
+//     reject('WRONG');
+//   }
+// });
+
+// myPromise.then(
+//   (value) => myDisplay(value),
+//   (error) => myDisplay(error)
+// );
